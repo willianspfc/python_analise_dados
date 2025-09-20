d2a1edcfdd31247f5c3120b9a0a54b329aa1b3c9
_willian86 <willianspfc@hotmail.com>
2025-09-20 11:34:07 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -16,6 +16,40 @@ import plotly.graph_objs as go
 from dash import Dash , html , dcc
 import numpy as np
 import config        # Nosso config.py
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans   
 from sklearn.preprocessing import StandardScaler
 
+app = Flask (__name__)
+pasta = config.FOLDER
+caminhoBD = config.DB_PATH
+rotas = config.ROTAS
+vazio = 0
+
+def init_db():
+    with sqlite3.connect(f'{pasta}{caminhoBD}') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS inadimplencia(
+                mes TEXT PRIMARY KEY
+                inadimplencia REAL)
+                       ''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS selic(
+                mes TEXT PRIMARY KEY
+                selic_diaria REAL)
+                       ''')
+        conn.commit()
+
+@app.route(rotas[0])
+def index():
+    return render_template_string(f'''
+        <h1> Upload de dados Economicos </h1>
+        <form action= "" method="POST" enctype="multipart/form-data">
+            <label for="campo_inadimplencia"> Arquivo de Inadimplencia (CSV): </label>
+            <input name="campo_inadimplencia" type="file" required>              
+
+            <label for="campo_selic"> Arquivo de Taxa Selic (CSV): </label>
+            <input name="campo_selic" type="file" required>       
+
+            <input type="">                                
+                                  ''')
